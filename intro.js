29745da16d3ebe80efd74f56572748bede9527dd
Ryan Siu <rsiu@stuy.edu>
2017-12-11 14:25:20 -0500

    Finish work 16

--- intro.js
+++ intro.js
@@ -33,3 +33,32 @@ var listElement = document.getElementsByTagName("li");
 for (le of listElement) {
   addListeners(le);
 }
+
+var fibonacci = function(n) {
+  var i = 0;
+  var j = 1;
+
+  while (n > 1) {
+    var temp = j;
+    j = i + j;
+    i = temp;
+    n--;
+  }
+
+  return i;
+}
+
+var getLenFib = function(list) {
+  return document.getElementsByClassName("fib").length;
+}
+
+var addFib = function() {
+  var list = document.getElementById("list2");
+  var li = document.createElement("li");
+  li.setAttribute("class", "fib")
+  list.appendChild(li);
+  li.innerHTML = fibonacci(getLenFib(list));
+}
+
+var button2 = document.getElementById("b2");
+b2.addEventListener( "click", addFib );
